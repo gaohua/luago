82e9424e52a59c7dc6ef379141e43f182e2818f7
gaohuaid <gaohuacode@gmail.com>
2022-01-25 10:48:56 +0800

    init ch13

---
+++ go/ch13/src/luago/vm/inst_Upvalue.go
@@ -0,0 +1,40 @@
+package vm
+
+import (
+	. "luago/api"
+)
+
+func getUpval(i Instruction, vm LuaVM) {
+	a, b, _ := i.ABC()
+	a += 1
+	b += 1
+
+	vm.Copy(LuaUpvalueIndex(b), a)
+}
+
+func setUpval(i Instruction, vm LuaVM) {
+	a, b, _ := i.ABC()
+	a += 1
+	b += 1
+
+	vm.Copy(a, LuaUpvalueIndex(b))
+}
+
+func getTabUp(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+	b += 1
+
+	vm.GetRK(c)
+	vm.GetTable(LuaUpvalueIndex(b))
+	vm.Replace(a)
+}
+
+func setTabUp(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+
+	vm.GetRK(b)
+	vm.GetRK(c)
+	vm.SetTable(LuaUpvalueIndex(a))
+}
