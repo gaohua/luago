12da7ec2130cb6e1478d84c6dc3e97545da19c8a
gaohuaid <gaohuacode@gmail.com>
2022-01-30 21:53:57 +0800

    add ch15

---
+++ go/ch15/src/luago/compiler/ast/stat.go
@@ -0,0 +1,59 @@
+package ast
+
+type Stat interface{}
+
+type EmptyStat struct{}              // ';'
+type BreakStat struct{ Line int }    //break
+type LabelStat struct{ Name string } // '::' Name '::'
+type GotoStat struct{ Name string }  // goto Name
+type DoStat struct{ Block *Block }   // do block end
+type FuncCallStat = FuncCallExp      // functioncall
+
+type WhileStat struct {
+	Exp   Exp
+	Block *Block
+}
+
+type RepeatStat struct {
+	Block *Block
+	Rxp   Exp
+}
+
+type IfStat struct {
+	Exps   []Exp
+	Blocks []*Block
+}
+
+type ForNumStat struct {
+	LineOfFor int
+	LineOfDo  int
+	VarName   string
+	InitExp   Exp
+	LimitExp  Exp
+	StepExp   Exp
+	Block     *Block
+}
+
+type ForInStat struct {
+	LineOfDo int
+	NameList []string
+	ExpList  []Exp
+	Block    *Block
+}
+
+type LocalVarDeclStat struct {
+	LastLine int
+	NameList []string
+	ExpList  []Exp
+}
+
+type AssignStat struct {
+	LastLine int
+	VarList  []Exp
+	ExpList  []Exp
+}
+
+type LocalFuncDefStat struct {
+	Name string
+	Exp  *FuncDefExp
+}
